7366fdcb7af16bb8bf550227d47896fb2142320c
stackOF <stackoverflovv@gmail.com>
2020-03-31 21:50:32 +0300

    create first files from lesson 4.1 step 6

---
+++ conftest.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    lng = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': lng})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
