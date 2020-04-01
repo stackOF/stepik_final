b0d7838f32183298dadcbff1d8ebe61f346c4382
stackOF <stackoverflovv@gmail.com>
2020-04-01 20:44:53 +0300

    create base and main pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
