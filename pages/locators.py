7886c4a2141eca4eaad032f1197b0770fcdda11c
stackOF <stackoverflovv@gmail.com>
2020-04-04 21:06:22 +0300

    Реализация LoginPage

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REG_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from selenium import webdriver
+from .locators import LoginPageLocators
+#from .locators import MainPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "'login' not in current url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form not found"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), "Registration form not found"
